24f8a4eb6675746ed010e1c958220a78a1219492
vanessalaureano <solytagamer@gmail.com>
2023-07-15 11:29:31 -0300

    update

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
 let listaDeItens = [];
 
-cliente = {
-    nome: 'Mariana', 
-    idade: 35,
-    genero: 'Feminino'
- }
+const form = document.getElementById("form-itens");
+const itensInput = document.getElementById("receber-item");
+const ulItens = document.getElementById("lista-de-itens");
 
-cliente.estado = 'Pernambuco'
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    salvarItem();
+    mostrarItem();
+})
 
-alert('Olá, meu nome é ' + cliente.nome + ' e moro no estado de ' + cliente.estado + '.')
+function salvarItem() {
+    const comprasItem = itensInput.value;
+    const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase === comprasItem.toUpperCase);
+
+    if (checarDuplicado) {
+        alert("O item já foi adicionado.")
+    } else {
+        listaDeItens.push({
+            valor: comprasItem
+        })
+    }
+}
+
+function mostrarItem() {
+    ulItens.innerHTML = '';
+    listaDeItens.forEach((elemento, index) => {
+        ulItens.innerHTML += `
+        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+        </li>
+        `
+    })
+}
