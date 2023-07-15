d2deedc177561f8a7fd72dff18ed2257c88b7cd0
vanessalaureano <solytagamer@gmail.com>
2023-07-15 10:11:44 -0300

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+let listaDeItens = [];
+
+cliente = {
+    nome: 'Mariana', 
+    idade: 35,
+    genero: 'Feminino'
+ }
+
+cliente.estado = 'Pernambuco'
+
+alert('Olá, meu nome é ' + cliente.nome + ' e moro no estado de ' + cliente.estado + '.')
